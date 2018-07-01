3d813c708df9275093ca065b966d6f14b3c4a61c
brian <brian@brianstein.co.uk>
2018-07-01 17:14:15 +0100

    added the start of vue quiz Signed-off-by: brian <brian@brianstein.co.uk>
    Signed-off-by: brian <brian@brianstein.co.uk>

---
+++ resources/assets/js/components/dictionary/flashcards/flashcards.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>jyutping</p>
+    <p>english x 3</p>
+    <p>sound</p>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                dictionary: [],
+                dictionary_length: 1,
+                cantonese: ""
+            }
+        },
+        methods: {
+            getwords() {
+                axios.get('/api/whole-dictionary', {
+                    params: {}
+                }).then((response) => {
+                    console.log(response)
+                    this.words = response.data.data;
+                    this.words.length = this.dictionary-length
+
+                })
+            },
+            mounted () {
+                this.getwords()
+            },
+        }
+    }
+</script>
