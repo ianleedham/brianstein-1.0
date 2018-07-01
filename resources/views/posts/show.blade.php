a4e941b3f7f42150ff9686cbf075b77977e99c50
brian <brian@brianstein.co.uk>
2018-07-01 11:50:58 +0100

    added checks to show posts Signed-off-by: brian <brian@brianstein.co.uk>
    Signed-off-by: brian <brian@brianstein.co.uk>

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -20,5 +20,15 @@
             {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
             {!!Form::close()!!}
         @endif
+        @can('edit', $post)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+        @endcan
+        @can('delete', $post)
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::close()!!}
+        @endcan
     @endif
 @endsection
