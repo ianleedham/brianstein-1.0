1b8e4c4ef1660d131f019ee8cc062095ebce8a6b
brian <brian@brianstein.co.uk>
2018-07-05 11:55:07 +0100

    editing email settings Signed-off-by: brian <brian@brianstein.co.uk>
    Signed-off-by: brian <brian@brianstein.co.uk>

--- vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -56,6 +56,8 @@ class ResetPassword extends Notification
         }
 
         return (new MailMessage)
+            ->from("admin@brianstein.co.uk", $name = "brianstein")
+            ->subject('Password Reset')
             ->line('You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
             ->line('If you did not request a password reset, no further action is required.');
