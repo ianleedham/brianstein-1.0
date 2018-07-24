b351cec039d0b70ea6b1a2fdfa4c11e03da711bf
brian <brian@brianstein.co.uk>
2018-07-24 12:47:40 +0100

    edit media server Signed-off-by: brian <brian@brianstein.co.uk>
    Signed-off-by: brian <brian@brianstein.co.uk>

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <p>This is a simple personal media server where users can upload a file and depending on the file type it will be placed into either documents, images or music.
-        Currently guests can only see a screenshot of the personal media server below</p>
-    <img style="width:80%;margin: 6rem " src="/storage/files/fileMediaServer2_1527370349.png">
+    
 
         {!! Form::open(['action' => 'FilesController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="form-group">
