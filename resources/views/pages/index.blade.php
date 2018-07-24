1e17e208aa01949f6aef492dcfbce590128b6a4b
brian <brian@brianstein.co.uk>
2018-07-24 12:27:28 +0100

    edit landing page Signed-off-by: brian <brian@brianstein.co.uk>
    Signed-off-by: brian <brian@brianstein.co.uk>

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,9 +6,9 @@
         <p> This website is designed on the Laravel framework which is a MVC PHP framework which makes it easy
             to produce beautiful secure websites quickly and easily. It does this by providing boiler plate code,
             and a multitude of features for anything you need. Some of the most important features are the
-            authentication system, the migrations for databases and the middle ware access controls.
-            On the navigation bar you will find a selection of projects and functions available on the website
-            and the languages protocols implemented in them.</p>
-        <h4>Please feel free to login to see the rest of the website</h4>
+            authentication system, the migrations for databases and the middleware access controls.
+            On the navigation bar you will find a selection of my projects and functions available on the website
+            and the protocols implemented.</p>
+        <h4>Please register in the top right corner to test out the website fully</h4>
     </div>
 @endsection
